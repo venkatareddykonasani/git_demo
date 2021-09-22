3e264181c0441de3917ab2c0a146f9dc2f08abd3
Venkat Reddy <21.venkat@gmail.com>
2021-09-22 10:35:11 +0530

    added code for mean and median units sold

--- sales_analysis_example1.py
+++ sales_analysis_example1.py
@@ -4,3 +4,9 @@ data_path="https://raw.githubusercontent.com/venkatareddykonasani/Datasets/maste
 Sales=pd.read_csv(data_path+"Sales_by_country_v1.csv")
 print(Sales.shape)
 print(Sales.info())
+
+mean_units=Sales["unitsSold"].mean()
+print(mean_units)
+
+median_units=Sales["unitsSold"].median()
+print(median_units)
